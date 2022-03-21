ecce0a5ca092a40b797a2fff3a0fe0d7d87e2228
Justin Herpin <justindherpin@yahoo.com>
2022-03-21 12:25:23 -0700

    fixed errorHandler.  Routes and controller working.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/middleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()

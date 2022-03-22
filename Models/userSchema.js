65b447651aa0a384cc04dfadddfd763644c3a09c
Justin Herpin <justindherpin@yahoo.com>
2022-03-21 17:14:18 -0700

    userController and userRoutes setup and connecting via postman.

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -3,9 +3,9 @@ const mongoose = require('../db/connection')
 
 const userSchema = new mongoose.Schema({
 
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { type: String, required: [true, 'Please add a name']},
+    email: { type: String, required: [true, 'Please add an email adress'], unique: true },
+    password: { type: String, required: [true, 'Please enter a password']},
     isAdmin: { type: Boolean, default: false },
     
     // students: [{ type: String, required: true}], // click on course add to their array?
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/courses', require('./routes/coursesRoutes'))
+app.use('/api/v1/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 

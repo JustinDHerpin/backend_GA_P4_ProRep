e1a324b7f1ce0e0cc2e7555e31a24a50f077cd93
Justin Herpin <justindherpin@yahoo.com>
2022-03-21 18:32:30 -0700

    user controller register user route works.

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,10 +1,50 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../Models/userSchema')
+
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
 
-const registerUser = (req, res) => {
-    res.json({ message: 'Register User' })
-}
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Missing name, email, or password.  Please try again.')
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('Sorry...User already exists.  Please try again.')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data.')
+    }
+})
 
 
 
@@ -12,9 +52,9 @@ const registerUser = (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({ message: 'Login User' })
-}
+})
 
 
 
@@ -22,9 +62,9 @@ const loginUser = (req, res) => {
 // @route   GET /api/users/me
 // @access  Public
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data display' })
-}
+})
 
 
 

8dcf594617ce760b5fe6e4a9255b8348bb4f95c6
Justin Herpin <justindherpin@yahoo.com>
2022-03-23 22:16:53 -0700

    end of day 3/23/22: small changes and added cors

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const port = process.env.PORT || 5000
-const mongoose = require('./db/connection')
+const express = require("express");
+const cors = require("cors");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+const mongoose = require("./db/connection");
 
-const app = express()
+const app = express();
 
 // const Course = require('./models/courseSchema.js')
 // const Wine = require('./models/wineSchema')
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use("/api/v1/courses", require("./routes/coursesRoutes"));
+app.use("/api/v1/users", require("./routes/userRoutes"));
 
-app.use('/api/v1/courses', require('./routes/coursesRoutes'))
-app.use('/api/v1/users', require('./routes/userRoutes'))
+app.use(errorHandler);
 
-app.use(errorHandler)
-
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+app.listen(port, () => console.log(`Server running on port: ${port}`));

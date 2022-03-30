936cd65c519f15a832402f8daddbe3f78cd07ba8
Justin Herpin <justindherpin@yahoo.com>
2022-03-30 15:35:27 -0700

    still cors error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("port", process.env.PORT || 5000);
 // const Course = require('./models/courseSchema.js')
 // const Wine = require('./models/wineSchema')
-app.use(cors());
+// app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
